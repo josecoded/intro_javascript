fad5abbae34d560b16468268f4bf7c820117565c
joseph joshua <105305220+josecoded@users.noreply.github.com>
2022-06-23 10:52:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let Name ="My name is Joseph Joshua";
+let height ="5.8fts";
+let country = "Nigeria";
+
+alert(Name);
+alert(height);
+alert(country);
+
+document.getElementById("Name").innerHTML = Name;
+document.getElementById("height").innerHTML = height;
+document.getElementById("country").innerHTML =country;
